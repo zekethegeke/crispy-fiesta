b091bac66f4f779a5e6ba5daef224fb9cd47ddcf
zekethegeke <zekethegeke@users.noreply.github.com>
2020-10-10 23:16:23 +0200

    table and group tags for declarative  syntax

--- tst/weke-content-decl.test.ts
+++ tst/weke-content-decl.test.ts
@@ -1,12 +1,11 @@
 import { wrap } from 'jest-snapshot-serializer-raw';
-import { Addresses, addressesPage } from '../src/Addresses';
-import { block, h3, uml } from 'weke-plex';
+import { Address, Addresses, addressesPage } from '../src/Addresses';
+import { block, group, h3, span, uml, list1, list2, tableByMap, tableOf, tableByColumns } from 'weke-plex';
 
 const addresses = (new Addresses(require("./test-addresses.json")));
+const males = addresses.males.map(it => `"${it.first}" -> "Male"`);
 
 test('yet another mark around language', () => {
-    const males = addresses.males.map(it => `"${it.first}" -> "Male"`);
-
     const content = [
         h3 `Purpose`,
         block `
@@ -16,5 +15,70 @@ test('yet another mark around language', () => {
             (*) - (*)
             ${males}`,
     ].join("\n");
+    expect(wrap(content)).toMatchSnapshot();
+});
+
+test('yet another mark around list', () => {
+    const dotComs = addresses.all.filter((it) => it.email.endsWith('.com'));
+    const ofMales = (addy: Address) => males.filter((it) => it.includes(addy.first));
+
+    const content = [
+        h3 `Lists`,
+        list1(males, (male) => 
+            span `* each male: ${male}`),
+
+        list2(dotComs, (dotCom) => 
+            [ span `* each email: ${dotCom.email}`, 
+                ofMales(dotCom)],
+            (male) => 
+                span `** each male: ${male}`),
+    ].join("\n");
+    expect(wrap(content)).toMatchSnapshot();
+});
+
+test('yet another mark around list', () => {
+    const dotComs = addresses.all.filter((it) => it.email.endsWith('.com'))
+        .slice(0, 10);
+
+    const content = [
+        h3 `tableOf`,
+        tableOf(dotComs),
+
+        h3 `tableOf filtered`,
+        tableOf(dotComs, ['first', 'email']),
+
+        h3 `tableByMap`,
+        //  tableOf
+        tableByMap(dotComs, (it) => {
+            return { 
+                "//First Name//": 
+                    span `cell: ${it.first}`,
+                "Last Name":
+                    span `cell: ${it.last}`,
+            }
+        }),
+
+        h3 `tableByColumns`,
+        tableByColumns({
+            rows: dotComs, 
+            columns: [{   
+                key: 'first', 
+                headOf: (key) => `//${key.toUpperCase()}//`,
+                cellOf: (data) => span `cell: ${data.value}`
+            },{   
+                key: 'last', 
+                headOf: (key, colIndex) => `//${key.toUpperCase()}#${colIndex}//`,
+                cellOf: (data) =>  group `
+                    multiline: ${data.value}
+                    ${data.rowIndex}:${data.colIndex}`
+            },{   
+                key: 'last', 
+                headOf: (key, colIndex) => `//${key.toUpperCase()}#${colIndex}//`,
+                cellOf: (data) =>  group `
+                    multiline: ${data.value}
+                    ${data.rowIndex}:${data.colIndex}`
+            },]
+        }),
+        ].join("\n");
     expect(wrap(content)).toMatchSnapshot();
 });
--- tst/weke-content-oop.test.ts
+++ tst/weke-content-oop.test.ts
@@ -3,6 +3,7 @@ import { Addresses, addressesPage } from '../src/Addresses';
 import { wrap } from 'jest-snapshot-serializer-raw';
 
 const addresses = (new Addresses(require("./test-addresses.json")));
+const males = addresses.males.map(it => `"${it.first}" -> "Male"`);
 
 test('all your object belong to us', () => {
     const doc = new WekeDocument();
@@ -12,9 +13,34 @@ test('all your object belong to us', () => {
     doc.appendElement(new WekeH3Element(`//[[${addressesPage.path}]]//`));
     const uml = new WekeUmlElement();
     uml.appendLines(["(*) - (*)"]);
-    uml.appendLines(addresses.males.map(it => `"${it.first}" -> "Male"`));
+    uml.appendLines(males);
     doc.appendElement(uml);
 
+    const serializer = new WekeDocumentSerializer();
+    expect(wrap(serializer.serializeToString(doc))).toMatchSnapshot();
+});
+
+test('all your list belong to us', () => {
+    const doc = new WekeDocument();
+    doc.appendElement(new WekeH3Element("Lists"));
+
+    // const list1 = new WekeListElement();
+    // males.forEach((male) => list1.appendLine(`each male: ${male}`));
+    // doc.appendElement(list1);
+    
+    // const dotComs = addresses.all.filter((it) => it.email.endsWith('.com'));
+    // const ofMales = (addy: Address) => males.filter((it) => it.includes(addy.first));
+    // const list2 = new WekeListElement();
+    // dotComs.forEach(dotCom => {
+    //     list2.appendLine(`each dotCom: ${dotCom.email}`);
+    //     const list3 = new WekeListElement();
+    //     ofMales(dotCom).forEach((male) => {
+    //         list3.appendLine(`each male: ${male}`);
+    //     });
+    //     list2.appendElement(list3);
+    // });
+    // doc.appendElement(list2);
+    
     const serializer = new WekeDocumentSerializer();
     expect(wrap(serializer.serializeToString(doc))).toMatchSnapshot();
 });
